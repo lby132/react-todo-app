80911714abdccdf9e6910a136795d7f09ef5cd3f
lby132 <quddbscjswow@naver.com>
2023-02-01 20:55:57 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
 import React, {useState} from "react";
 import "./App.css";
+import List from "./components/List";
+import Form from "./components/Form";
 
 export default function App() {
 
     const [todoData, setTodoData] = useState([]);
     const [value, setValue] = useState("");
-
-    const btnStyle = {
-        color: "#fff",
-        border: "none",
-        padding: "5px, 9px",
-        borderRadius: "50%",
-        cursor: "pointer",
-        float: "right"
-    }
-
-    const getStyle = (completed) => {
-        return {
-            padding: "10px",
-            borderBottom: "1px #ccc dotted",
-            textDecoration: completed ? "line-through" : "none",
-
-        }
-    }
-
-    const handleChange = (e) => {
-        setValue(e.target.value);
-    }
-
-    const handleClick = (id) => {
-        console.log(id)
-        let newTodoData = todoData.filter(data => data.id !== id);
-        console.log('newTodoData ', newTodoData);
-        setTodoData(newTodoData);
-    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -48,17 +21,8 @@ export default function App() {
         //원래 있던 할 일에 새로운 할 일 더해주기
         setTodoData(prev => [...prev, newTodo]);
         setValue("");
-    }
+    };
 
-    const handleCompleteChange = (id) => {
-        let newTodoData = todoData.map(data => {
-            if (data.id === id) {
-                data.completed = !data.completed;
-            }
-            return data;
-        });
-        setTodoData(newTodoData);
-    }
       return (
           <div className="container">
               <div className="todoBlock">
@@ -66,33 +30,9 @@ export default function App() {
                       <h1>할 일 목록</h1>
                   </div>
 
-                  {todoData.map((data) => {
-                      return (
-                          <div style={getStyle(data.completed)} key={data.id}>
-                              <input type="checkbox" defaultChecked={false} onChange={() => handleCompleteChange(data.id)} />
-                              {data.title}
-                              <button style={btnStyle} onClick={() => handleClick(data.id)}>x</button>
-                          </div>
-                      );
-                  })}
+                  <List todoData={todoData} setTodoData={setTodoData} />
 
-                  <form style={{display: 'flex'}} onSubmit={handleSubmit}>
-                      <input
-                          type="text"
-                          name="value"
-                          style={{flex: '10', padding: '5px'}}
-                          placeholder="해야 할 일을 입력하세요."
-                          value={value}
-                          onChange={handleChange}
-                      />
-                      <input
-                        type="submit"
-                        value="입력"
-                        className="btn"
-                        style={{flex: '1'}}
-                      />
-                  </form>
-
+                  <Form handleSubmit={handleSubmit} value={value} setValue={setValue}/>
               </div>
           </div>
       );
